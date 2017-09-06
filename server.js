856e49e9162cb32839296923a3e54af5a7f10198
antoncda <30837837+antoncda@users.noreply.github.com>
2017-09-06 16:23:18 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,28 +5,26 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
+var articles = {
+ articleOne: {
     title: 'Article one | Anton',
     header: 'Antons Article',
     date: ' Sep 2017',
     article: '<p> This is my first Article which introduces my Serverside JS.</p>;'
-};
-
-var articleTwo = {
+},
+ articleTwo: {
     title: 'Article Two | Anton',
     header: 'Antons Article Article Two',
     date: ' Sep 2017',
     article: '<p> This is my second Article which introduces my Serverside JS.</p>;'
-};
-
-var articleThree = {
+},
+ articleThree: {
     title: 'Article Three | Anton',
     header: 'Antons Article Three',
     date: ' Sep 2017',
     article: '<p> This is my third Article which introduces my Serverside JS.</p>;'
-};
-
-
+}
+}
 
 function siteCreate(data) {
     title = data.title;
@@ -77,10 +75,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/:articleName', function (req, res) {
    //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
    //res.send("hello article one");
-   res.send(siteCreate(articleOne));
+   res.send(siteCreate(articles[articleName]));
    
     
 });
